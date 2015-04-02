6ffe359a6361a06d96b146ef96cfba0c95241650
Henry-H-Yan <henryyan829@gmail.com>
2015-04-02 16:47:46 -0400

    DP question

---
+++ src/project_Euler/p011.java
@@ -0,0 +1,19 @@
+package project_Euler;
+
+public class p011 {
+	final static int TOTAL = 200;
+
+	public static void main(String[] args) {
+	    int[] coins = {1, 2, 5, 10, 20, 50, 100, 200};
+	    int[] ways = new int[TOTAL + 1];
+	    ways[0] = 1;
+
+	    for (int coin : coins) {
+	        for (int j = coin; j <= TOTAL; j++) {
+	            ways[j] += ways[j - coin];
+	        }
+	    }
+
+	    System.out.println("Result: " + ways[TOTAL]);
+	}
+}
