4e20acdd7f16fe977209e790eeb2379d9526caf8
Henry-H-Yan <henryyan829@gmail.com>
2015-04-25 20:17:20 -0400

    new code

--- src/project_Euler/p097.java
+++ src/project_Euler/p097.java
@@ -1,5 +1,17 @@
 package project_Euler;
+import java.math.BigInteger;
 
-public class p097 {
+
+public final class p097  {
+
+	public static void main(String[] args) {
+		BigInteger modulus = BigInteger.TEN.pow(10);
+		BigInteger n = BigInteger.valueOf(2);
+		n=n.modPow(BigInteger.valueOf(7830457), modulus);
+		
+		n = n.multiply(BigInteger.valueOf(28433)).mod(modulus);
+		n = n.add(BigInteger.ONE).mod(modulus);
+		System.out.println( String.format("%010d", n));
+	}
 
 }
