815606131e3ac8b386bc2bbbe85c43a04ee834e1
Henry-H-Yan <henryyan829@gmail.com>
2015-04-27 18:02:58 -0400

    code change

--- src/project_Euler/p004.java
+++ src/project_Euler/p004.java
@@ -13,11 +13,9 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 public class p004 {
 
 	public static void main(String[] args) {
-		System.out.println(new p004().run());
-	}
 
 
-	public String run() {
+
 		int maxPalin = -1;
 		for (int i = 100; i < 1000; i++) {
 			for (int j = 100; j < 1000; j++) {
@@ -25,10 +23,10 @@ public class p004 {
 				if (isPalindrome(String.valueOf(cur) )) maxPalin = maxPalin>=cur? maxPalin:cur;
 			}
 		}
-		return Integer.toString(maxPalin);
+		System.out.println(Integer.toString(maxPalin));
 	}
 
-	public boolean isPalindrome(String s){
+	public static boolean isPalindrome(String s){
 		if (s.length()<=1) return true;
 		if(s.charAt(0)==s.charAt(s.length()-1)) return isPalindrome(s.substring(1, s.length()-1));
 		return false;
