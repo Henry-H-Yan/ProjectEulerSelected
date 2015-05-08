039d72c717577400cf6e63d8421c8c76a2f18be6
Henry-H-Yan <henryyan829@gmail.com>
2015-05-07 23:12:09 -0700

    code change

--- src/project_Euler/p018.java
+++ src/project_Euler/p018.java
@@ -1,5 +1,38 @@
 package project_Euler;
 
-public class p018 {
-
+public final class p018   {
+	
+	public static void main(String[] args) {
+	
+	// apply the idea of dynamic programming
+		int [] [] copy = triangle.clone();
+		
+		for (int i = triangle.length - 2; i >= 0; i--) {
+			for (int j = 0; j < triangle[i].length; j++)
+				// recurrance relation
+				//triangle[i][j] = triangle[i][j]+ Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]); 
+				copy[i][j] = copy[i][j]+ Math.max(copy[i + 1][j], copy[i + 1][j + 1]); 
+		}
+		System.out.println(  Integer.toString(triangle[0][0]));
+	}
+	
+	
+	static private int[][] triangle = {
+		{75},
+		{95,64},
+		{17,47,82},
+		{18,35,87,10},
+		{20,04,82,47,65},
+		{19,01,23,75,03,34},
+		{88,02,77,73,07,63,67},
+		{99,65,04,28,06,16,70,92},
+		{41,41,26,56,83,40,80,70,33},
+		{41,48,72,33,47,32,37,16,94,29},
+		{53,71,44,65,25,43,91,52,97,51,14},
+		{70,11,33,28,77,73,17,78,39,68,17,57},
+		{91,71,52,38,17,14,91,43,58,50,27,29,48},
+		{63,66,04,68,89,53,67,30,73,16,69,87,40,31},
+		{04,62,98,27,23, 9,70,98,73,93,38,53,60,04,23},
+	};
+	
 }
